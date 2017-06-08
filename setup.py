be57ddda39211ad3e871c9215e919b8d40000568
Alessio da Sasha <acuzzio@gmail.com>
2017-06-08 16:56:01 +0200

    Numba added to imports

--- setup.py
+++ setup.py
@@ -23,6 +23,6 @@ setup(
         'Topic :: Scientific/Engineering :: Chemistry'
     ],
     #scripts=['Scripts/PutScripthere.py'],
-    install_requires=['h5py', 'numpy', 'scipy'],
+    install_requires=['h5py', 'numpy', 'scipy', 'numba'],
     extras_require={'test': ['nose', 'coverage']}
 )
