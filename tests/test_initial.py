cc272726148ae2ba0d5553cc8f26fcc7ffeff233
Alessio da Sasha <acuzzio@gmail.com>
2017-06-08 18:07:18 +0200

    Test folder finally added.

---
+++ tests/test_initial.py
@@ -0,0 +1,14 @@
+from quantumpropagator import astridParser
+
+def test_astridParser():
+    '''
+    It tests for the array shapes returned by the parser of Astrid data ...
+    '''
+    (dist,newEne,newDipo,newNAC,newGac) = astridParser(4,400,'tests/test_files/ast2')
+    test1 = dist.shape == (400,)
+    test2 = newEne.shape == (400, 4)
+    test3 = newDipo.shape == (400, 1, 4, 4)
+    test4 = newNAC.shape == (400, 4, 4)
+    test5 = newGac.shape == (400, 4, 4)
+    assert all([test1,test2,test3,test4,test5])
+
