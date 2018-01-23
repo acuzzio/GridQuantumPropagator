cfde8096c88330ed73dc87fce155814f8be40b6b
Alessio da Sasha <acuzzio@gmail.com>
2018-01-23 16:38:17 +0100

    Parallel postProcessing done

--- Scripts/PostProcessing3dData.py
+++ Scripts/PostProcessing3dData.py
@@ -7,7 +7,8 @@ from collections import namedtuple
 from argparse import ArgumentParser
 
 import glob
-
+from itertools import repeat
+import multiprocessing
 import numpy as np
 import os
 
@@ -30,26 +31,31 @@ def read_single_arguments(single_inputs):
                         type=str,
                         required=True,
                         help="The location of the Grid folder")
+    parser.add_argument("-p", "--parallel",
+                        dest="p",
+                        type=int,
+                        help="number of processors if you want it parallel")
+
     args = parser.parse_args()
     if args.o != None:
         single_inputs = single_inputs._replace(outF=args.o)
     if args.s != None:
         single_inputs = single_inputs._replace(glob=args.s)
+    if args.p != None:
+        single_inputs = single_inputs._replace(proc=args.p)
+
     return single_inputs
 
-def parseNAC(fileOut):
-    '''
-    I use bash here to quickly get out NAC values
-    fileOut :: filePath <- the output of molcas
-    '''
-    return(np.empty(10))
 
-
-def createOutputFile(fold,outFol):
+#def createOutputFile(fold,outFol):
+def createOutputFile(tupleI):
     '''
     given a projectname, creates single outFile (not yet corrected)
-    fold :: String <- the folder of a single project
+    fold :: String <- the folder of a single calculation
+    outFol :: String <- the folder where the output is collected
     '''
+    (fold,outFol) = tupleI
+    print('doing ' + fold)
     folder = os.path.dirname(fold)
     proj = os.path.basename(fold)
     root = folder +'/' + proj + '/' + proj
@@ -73,7 +79,6 @@ def createOutputFile(fold,outFol):
                                'SFS_TRANSITION_DENSITIES'])
         overlap = retrieve_hdf5_data(h5rassi,'ORIGINAL_OVERLAPS')
         NAC = parseNAC(h5out)
-
         outfile = oroot + '.all.h5'
 
         outTuple = [('CENTER_COORDINATES', geom),
@@ -81,48 +86,80 @@ def createOutputFile(fold,outFol):
                     ('CI_VECTORS', ciVect),
                     ('ROOT_ENERGIES', ener),
                     ('DIPOLES', dipoles[0:3]),
-                    ('TRANDENS',transDen),
+#                    ('TRANDENS',transDen),
                     ('OVERLAP', overlap),
                     ('NAC', NAC)]
 
-        writeH5file(outfile,outTuple)
-        print('File ' + outfile + ' written.')
+        try:
+           writeH5file(outfile,outTuple)
+        except "Unable to open file":
+           print(outfile)
+        log += ' -> ' + str(NAC.size)
+        #print('File ' + outfile + ' written.')
     else:
         log = proj + ': this calculation is not completed'
         boolean = False
     return(log,boolean)
 
-single_inputs = namedtuple("single_input", ("glob","outF"))
+
+def parseNAC(fileN):
+    '''
+    I use bash here to quickly get out NAC values
+    fileOut :: filePath <- the output of molcas
+    '''
+    import subprocess
+    command = "grep norm: " + fileN + "| awk '{print $2}'"
+    p = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
+    (output, err) = p.communicate()
+    outputO = np.array([float(x) for x in output.split(b'\n') if x != b'' ])
+    return(outputO)
+
+def unpackThingsFromParallel(listOfval):
+    '''
+    this unpacks the result list of tuples from the parallel execution
+    listOfval :: (String,Boolean) <- all the messages and values packed in
+    tuples
+    '''
+    log = '\n'.join([x[0] for x in listOfval])
+    length = len(listOfval)
+    trues = [x[1] for x in listOfval].count(True)
+    return(log,trues,length-trues)
+
+single_inputs = namedtuple("single_input", ("glob","outF","proc"))
+
 
 def main():
     '''
     from a grid frolder to a folder ready to be propagated
     '''
-    o_inputs = single_inputs("","")
+    o_inputs = single_inputs("","",1)
     inp = read_single_arguments(o_inputs)
     folders = npArrayOfFiles(inp.glob)
-    fold = folders[0:2]
-    logAll = ""
-    finished = 0
-    problems = 0
-    for i in fold:
-        (log,boolean) = createOutputFile(i,inp.outF)
-        logAll += '\n' + log
-        if boolean:
-            finished += 1
-        else:
-            problems += 1
+    fold = folders[:] # this does nothing, but I use it to try less files at the time
+    ####     serial Function
+    #for i in fold:
+    #    (log,boolean) = createOutputFile(i,inp.outF)
+    #    logAll += '\n' + log
+    #    if boolean:
+    #        finished += 1
+    #    else:
+    #        problems += 1
+    #### go parallel here
+    pool = multiprocessing.Pool(processes = inp.proc)
+    resultP = pool.map(createOutputFile, zip(fold, repeat(inp.outF)))
+    (logAll,finished,problems) = unpackThingsFromParallel(resultP)
     results = '''
 {}
 
 finished -> {}
 problems -> {}
 '''.format(logAll,finished,problems)
+
+    with open('report','w') as f:
+        f.write(results)
     print(results)
 
 
 if __name__ == "__main__":
         main()
-
-
 
