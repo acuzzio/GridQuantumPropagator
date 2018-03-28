419e4cdbfac4117510acd2cb7f730993bf87e47b
Alessio da Sasha <acuzzio@gmail.com>
2018-03-28 10:56:18 +0200

    Derivatives of g online...

--- src/quantumpropagator/KineticMadness.py
+++ src/quantumpropagator/KineticMadness.py
@@ -89,7 +89,7 @@ def calc_g_G(phi,gam,the):
     g_12 = dot(dp_phi,dg_gam)
     g_13 = dot(dp_phi,dt_the)
     det_g = g_11 * g_22 * g_33 - g_12**2 * g_33 + g_13**2 * g_22
-    print('det: {}'.format(det_g))
+    print('\nDet:\n{}'.format(det_g))
 
     # G matrix
     G_11 = (g_33 * g_22)/det_g
@@ -98,7 +98,49 @@ def calc_g_G(phi,gam,the):
     G_22 = (g_33 * g_11 - g_13**2)/det_g
     G_23 = (g_13 * g_12)/det_g
     G_33 = (g_22 * g_11 - g_12**2)/det_g
-    print('{} {} {} {} {} {}'.format(G_11,G_12,G_13,G_22,G_23,G_33))
+    print('\nG elements:\n {} {} {} {} {} {}'.format(G_11,G_12,G_13,G_22,G_23,G_33))
+
+    '''
+
+    derivatives. This will be funny.
+    VIM magic, right here...
+
+:%s/^ *//
+:%s/\n//
+:%s/\^/**/g
+:%s/Cos/cos/g
+:%s/Sin/sin/g
+:%s/\]/)/g
+:%s/\[/(/g
+:%s/\([0-9]\) (/\1 * (/g
+:%s/\([0-9]\) cos/\1 * cos/g
+:%s/\([0-9]\) sin/\1 * sin/g
+:%s/\([0-9]\) gam/\1 * gam/g
+:%s/\([0-9]\) the/\1 * gam/g
+:%s/) (/) * (/g
+:%s/cc (/cc * (/g
+:%s/ch (/ch * (/g
+:%s/cc d/cc * d/g
+:%s/ch d/ch * d/g
+:%s/) cos/) * cos/g
+:%s/) sin/) * sin/g
+:.s/d/dp_/g
+
+    '''
+    dgdets = 32 * (cc**2 + ch**2) * cos(gam) * (-2 * (cc**2 + ch**2) * (dp_c8x**2 + dp_c8y**2 + dp_c8z**2 + dp_c9x**2 + dp_c9y**2 +dp_c9z**2 + dp_h12x**2 + dp_h12y**2 + dp_h12z**2 + dp_h13x**2 + dp_h13y**2 + dp_h13z**2) * sin(gam) + sin(gam) * ((cc * (dp_c8x - dp_c9x) + ch * (dp_h12x - dp_h13x)) * cos(the) - (cc * (dp_c8z + dp_c9z) + ch * (dp_h12z + dp_h13z)) * sin(the))**2 + cos(gam) * ((cc * (dp_c8y + dp_c9y) + ch * (dp_h12y + dp_h13y)) * sin(gam) + cos(gam) * ((cc * (dp_c8z + dp_c9z) + ch * (dp_h12z + dp_h13z)) * cos(the) + (cc * dp_c8x - cc * dp_c9x + ch * dp_h12x - ch * dp_h13x) * sin(the))) * ((cc * (dp_c8y + dp_c9y) + ch * (dp_h12y + dp_h13y)) * cos(gam) + sin(gam) * (-(cc * (dp_c8z + dp_c9z) + ch * (dp_h12z + dp_h13z)) * cos(the) + (-cc * dp_c8x + cc * dp_c9x - ch * dp_h12x + ch * dp_h13x) * sin(the))) + sin(gam) * ((cc * (dp_c8y + dp_c9y) + ch * (dp_h12y + dp_h13y)) * cos(gam) + sin(gam) * (-(cc * (dp_c8z + dp_c9z) + ch * (dp_h12z + dp_h13z)) * cos(the) + (-cc * dp_c8x + cc * dp_c9x - ch * dp_h12x + ch * dp_h13x) * sin(the)))**2)
+
+    dtdets = -32 * (cc**2 + ch**2) * cos(gam)**3 * ((cc * (-dp_c8x + dp_c9x) + ch * (-dp_h12x + dp_h13x)) * cos(the) + (cc * (dp_c8z + dp_c9z) + ch * (dp_h12z + dp_h13z)) * sin(the)) * ((cc * (dp_c8y + dp_c9y) + ch * (dp_h12y + dp_h13y)) * sin(gam) + cos(gam) * ((cc * (dp_c8z + dp_c9z) + ch * (dp_h12z + dp_h13z)) * cos(the) + (cc * dp_c8x - cc * dp_c9x + ch * dp_h12x - ch * dp_h13x) * sin(the)))
+
+    dgdgdets = 32 * (cc**2 + ch**2) * (-2 * (cc**2 + ch**2) * (dp_c8x**2 + dp_c8y**2 + dp_c8z**2 + dp_c9x**2 + dp_c9y**2 + dp_c9z**2 + dp_h12x**2 + dp_h12y**2 + dp_h12z**2 + dp_h13x**2 + dp_h13y**2 + dp_h13z**2) * cos(gam)**2 + (cc * (dp_c8y + dp_c9y) + ch * (dp_h12y + dp_h13y))**2 * cos(gam)**4 + 2 * (cc**2 + ch**2) * (dp_c8x**2 + dp_c8y**2 + dp_c8z**2 + dp_c9x**2 + dp_c9y**2 + dp_c9z**2 + dp_h12x**2 + dp_h12y**2 + dp_h12z**2 + dp_h13x**2 + dp_h13y**2 + dp_h13z**2) * sin(gam)**2 - 5 * (cc * (dp_c8y + dp_c9y) + ch * (dp_h12y + dp_h13y))**2 * cos(gam)**2 * sin(gam)**2 - 4 * (cc * (dp_c8y + dp_c9y) + ch * (dp_h12y + dp_h13y)) * cos(gam)**3 * sin(gam) * ((cc * (dp_c8z + dp_c9z) + ch * (dp_h12z + dp_h13z)) * cos(the) + (cc * (dp_c8x - dp_c9x) + ch * (dp_h12x - dp_h13x)) * sin(the)) - cos(gam)**4 * ((cc * (dp_c8z + dp_c9z) + ch * (dp_h12z + dp_h13z)) * cos(the) + (cc * (dp_c8x - dp_c9x) + ch * (dp_h12x - dp_h13x)) * sin(the))**2 + 5 * cos(gam)**2 * sin(gam)**2 * ((cc * (dp_c8z + dp_c9z) + ch * (dp_h12z + dp_h13z)) * cos(the) + (cc * (dp_c8x - dp_c9x) + ch * (dp_h12x - dp_h13x)) * sin(the))**2 + 4 * (cc * (dp_c8y + dp_c9y) + ch * (dp_h12y + dp_h13y)) * cos(gam)**3 * sin(gam) * (-(cc * (dp_c8z + dp_c9z) + ch * (dp_h12z + dp_h13z)) * cos(the) + (cc * (-dp_c8x + dp_c9x) + ch * (-dp_h12x + dp_h13x)) * sin(the)) - 4 * (cc * (dp_c8y + dp_c9y) + ch * (dp_h12y + dp_h13y)) * cos(gam) * sin(gam)**3 * (-(cc * (dp_c8z + dp_c9z) + ch * (dp_h12z + dp_h13z)) * cos(the) + (cc * (-dp_c8x + dp_c9x) + ch * (-dp_h12x + dp_h13x)) * sin(the)) + cos(gam)**2 * ((cc * (dp_c8x - dp_c9x) + ch * (dp_h12x - dp_h13x)) * cos(the) - (cc * (dp_c8z + dp_c9z) + ch * (dp_h12z + dp_h13z)) * sin(the))**2 - sin(gam)**2 * ((cc * (dp_c8x - dp_c9x) + ch * (dp_h12x - dp_h13x)) * cos(the) - (cc * (dp_c8z + dp_c9z) + ch * (dp_h12z + dp_h13z)) * sin(the))**2 + cos(gam)**2 * ((cc * (dp_c8y + dp_c9y) + ch * (dp_h12y + dp_h13y)) * cos(gam) + sin(gam) * (-(cc * (dp_c8z + dp_c9z) + ch * (dp_h12z + dp_h13z)) * cos(the) + (-cc * dp_c8x + cc * dp_c9x - ch * dp_h12x + ch * dp_h13x) * sin(the)))**2 - sin(gam)**2 * ((cc * (dp_c8y + dp_c9y) + ch * (dp_h12y + dp_h13y)) * cos(gam) + sin(gam) * (-(cc * (dp_c8z + dp_c9z) + ch * (dp_h12z + dp_h13z)) * cos(the) + (-cc * dp_c8x + cc * dp_c9x - ch * dp_h12x + ch * dp_h13x) * sin(the)))**2)
+
+    dtdgdets = 32 * (cc**2 + ch**2) * cos(gam)**2 * ((cc * (-dp_c8x + dp_c9x) + ch * (-dp_h12x + dp_h13x)) * cos(the) + (cc * (dp_c8z + dp_c9z) + ch * (dp_h12z + dp_h13z)) * sin(the)) * (-(cc * (dp_c8y + dp_c9y) + ch * (dp_h12y + dp_h13y)) * (-1 + 2 * cos(2 * gam)) + 2 * (cc * (dp_c8z + dp_c9z) + ch * (dp_h12z + dp_h13z)) * cos(the) * sin(2 * gam) + 2 * (cc * (dp_c8x - dp_c9x) + ch * (dp_h12x - dp_h13x)) * sin(2 * gam) * sin(the))
+
+    dgdtdets = dtdgdets
+
+    dtdtdets = 32 * (cc**2 + ch**2) * cos(gam)**3 * (cos(gam) * ((cc * (dp_c8x - dp_c9x) + ch * (dp_h12x - dp_h13x)) * cos(the) - (cc * (dp_c8z + dp_c9z) + ch * (dp_h12z + dp_h13z)) * sin(the))**2 - ((cc * (dp_c8z + dp_c9z) + ch * (dp_h12z + dp_h13z)) * cos(the) + (cc * (dp_c8x - dp_c9x) + ch * (dp_h12x - dp_h13x)) * sin(the)) * ((cc * (dp_c8y + dp_c9y) + ch * (dp_h12y + dp_h13y)) * sin(gam) + cos(gam) * ((cc * (dp_c8z + dp_c9z) + ch * (dp_h12z + dp_h13z)) * cos(the) + (cc * dp_c8x - cc * dp_c9x + ch * dp_h12x - ch * dp_h13x) * sin(the))))
+
+    print('\nDerivatives g:\n{} {} {} {} {} {}'.format(dtdets,dgdets,dgdgdets,dtdtdets,dtdgdets,dgdtdets))
+
 
 def all_of_them():
     thetas = [130.000,129.101,128.202,127.303,126.404,125.506,124.607,123.708,122.809,121.910,121.011,120.112,119.213,118.315,117.416,116.517,115.618,114.719,113.820,112.921,112.022,111.124,110.225,109.326,108.427,107.528,106.629,105.730,104.831,103.933,103.034,102.135,101.236,100.337,099.438,098.539,097.640,096.742,095.843,094.944,094.045,093.146,092.247,091.348,090.449,089.551,088.652,087.753,086.854,085.955,085.056,084.157,083.258,082.360,081.461,080.562,079.663,078.764,077.865,076.966,076.067,075.169,074.270,073.371,072.472,071.573,070.674,069.775,068.876,067.978,067.079,066.180,065.281,064.382,063.483,062.584,061.685,060.787,059.888,058.989,058.090,057.191,056.292,055.393,054.494,053.596,052.697,051.798,050.899,050.000]
