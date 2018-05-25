10075b9741b33dd649856cc87f88357ae6c520c6
Alessio da Sasha <acuzzio@gmail.com>
2018-05-25 17:54:37 +0200

    travis0.1

--- src/quantumpropagator/GeneralFunctions.py
+++ src/quantumpropagator/GeneralFunctions.py
@@ -38,7 +38,7 @@ def printProgressBar(iteration, total, prefix='', suffix='', decimals=1, bar_len
     str_format = "{0:." + str(decimals) + "f}"
     percents = str_format.format(100 * (iteration / float(total)))
     filled_length = int(round(bar_length * iteration / float(total)))
-    bar = '█' * filled_length + '-' * (bar_length - filled_length)
+    bar = '*' * filled_length + '-' * (bar_length - filled_length)
 
     sys.stdout.write('\r%s |%s| %s%s %s' % (prefix, bar, percents, '%', suffix)),
     if iteration == total:
